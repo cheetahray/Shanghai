8520060b061e0163e74409e4082c5c0f771d849b
cheetahray <pi@raspberrypi.(none)>
2015-08-04 17:26:48 +0800

    Pitch Detection Done

---
+++ raynum.py
@@ -0,0 +1,25 @@
+import numpy
+import pyaudio
+import analyse
+
+# Initialize PyAudio
+pyaud = pyaudio.PyAudio()
+CHUNK = 8192
+
+# Open input stream, 16-bit mono at 44100 Hz
+# On my system, device 2 is a USB microphone, your number may differ.
+stream = pyaud.open(
+    format = pyaudio.paInt16,
+    channels = 1,
+    rate = 44100,
+    input_device_index = 0,
+    input = True,
+    frames_per_buffer=CHUNK)
+
+while True:
+    # Read raw microphone data
+    rawsamps = stream.read(CHUNK)
+    # Convert raw data to NumPy array
+    samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+    # Show the volume and pitch
+    print (analyse.loudness(samps), analyse.musical_detect_pitch(samps))
