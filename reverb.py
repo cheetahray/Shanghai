2013b9955192c703fea8c46d7b631b9e2f655d8a
cheetahray <pi@raspberrypi.(none)>
2015-08-18 14:00:50 +0000

    reverb sonic-pi ok

--- reverb.py
+++ reverb.py
@@ -2,10 +2,13 @@ import numpy
 import pyaudio
 import analyse
 import commands
+import time
+import math
 
 # Initialize PyAudio
 pyaud = pyaudio.PyAudio()
 CHUNK = 8192
+lastfeq = 0
 
 # Open input stream, 16-bit mono at 44100 Hz
 # On my system, device 2 is a USB microphone, your number may differ.
@@ -13,7 +16,7 @@ stream = pyaud.open(
     format = pyaudio.paInt16,
     channels = 1,
     rate = 44100,
-    input_device_index = 0,
+    input_device_index = 1,
     input = True,
     frames_per_buffer=CHUNK)
 
@@ -24,7 +27,13 @@ while True:
     samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
     # Show the volume and pitch
     rayfeq = analyse.musical_detect_pitch(samps)
-    if rayfeq > 0:
-        print (analyse.loudness(samps), rayfeq)
+    if rayfeq > 0 and math.fabs(rayfeq-lastfeq) > 2:
+        print(stream.stop_stream())
         title=commands.getoutput("echo \"with_fx :reverb, mix: 0.9, phase: 0.25, room: 1 do sample :guit_em9, rate: 0.5 end\" | sonic_pi")
-
+        time.sleep(30)
+        title=commands.getoutput("echo \"stop\" | sonic_pi")
+        print(stream.start_stream())
+        lastfeq = rayfeq
+        print (analyse.loudness(samps), rayfeq)
+    else:
+        lastfeq = 0
