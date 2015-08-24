2abcaa3e5eb958c8cdb3850ac366ba6c17ed4d47
cheetahray <pi@raspberrypi.(none)>
2015-08-24 11:44:00 +0000

    Need pressure test

--- wavtest.py
+++ wavtest.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import numpy
 import pyaudio
 import analyse
@@ -6,43 +7,53 @@ import time
 import math
 import wave
 
-# Initialize PyAudio
+chunk = 1024
+#wf = wave.open('thesong.wav', 'rb')
 pyaud = pyaudio.PyAudio()
-CHUNK = 8192
 lastfeq = 0
 
-# Open input stream, 16-bit mono at 44100 Hz
-# On my system, device 2 is a USB microphone, your number may differ.
+#print(pyaud.get_format_from_width(wf.getsampwidth()))
+#print(wf.getnchannels())
+#print(wf.getframerate())
+
 stream = pyaud.open(
     format = pyaudio.paInt16,
     channels = 1,
     rate = 44100,
-    input_device_index = 0,
     input = True,
-    output_device_index = 1,
-    output = True,
-    frames_per_buffer=CHUNK)
+    output = True)
 
 while True:
     # Read raw microphone data
-    rawsamps = stream.read(CHUNK)
+    rawsamps = stream.read(chunk)
     # Convert raw data to NumPy array
     samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
-    # Show the volume and pitch
     rayfeq = analyse.musical_detect_pitch(samps)
+    #print (analyse.loudness(samps), rayfeq)
     if rayfeq > 0 and math.fabs(rayfeq-lastfeq) > 2:
         print (analyse.loudness(samps), rayfeq)
-        #stream.stop_stream()
         wf = wave.open('thesong.wav', 'rb')
-        data = wf.readframes(CHUNK)
+        stream.close()
+        stream = pyaud.open(
+            format = pyaud.get_format_from_width(wf.getsampwidth()),
+            channels = wf.getnchannels(),
+            rate = wf.getframerate(),
+            input = False,
+            output = True)
+        data = wf.readframes(chunk)
         while data != '':
             stream.write(data)
-            data = wf.readframes(CHUNK)
+            data = wf.readframes(chunk)
         wf.close()
-        #title=commands.getoutput("echo \"with_fx :reverb, mix: 0.9, phase: 0.25, room: 1 do sample :guit_em9, rate: 0.5 end\" | sonic_pi")
-        time.sleep(10)
-        #title=commands.getoutput("echo \"stop\" | sonic_pi")
-        #stream.start_stream()
+        stream.close() 
+        stream = pyaud.open(
+            format = pyaudio.paInt16,
+            channels = 1,
+            rate = 44100,
+            input = True,
+            output = False)
         lastfeq = rayfeq
     else:
         lastfeq = 0
+
+pyaud.terminate()
