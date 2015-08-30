023c1e2265ad4640266586df1757e2e8bbcec9cf
cheetahray <pi@raspberrypi.(none)>
2015-08-30 13:23:17 +0000

    put time.sleep(wav len) can solve s.gui, wtf

--- raydsp.py
+++ raydsp.py
@@ -1,21 +1,8 @@
 from pyo import *
-import time
-from threading import Timer
-import pyautogui
-
-def print_time():
-    global s
-    s.gui()
-
-def play_wav(raywav):
-    a.setPath(raywav)
-    b.setInput(a) 
-    threading.Timer(0, print_time, ()).start()
-    time.sleep(10)  # sleep while time-delay events execute
-    pyautogui.hotkey('ctrl', 'q')
 
 s = Server(audio="jack").boot()
 s.start()
 a = SfPlayer("/home/pi/Shanghai/wav/Strat F- 52.wav", loop=False, mul=.4)
 b = Freeverb(a, size=[.79,.8], damp=.9, bal=.3).out()
-play_wav("/home/pi/Shanghai/wav/Strat F- 82.wav")
+time.sleep(10)
+print("ray")
