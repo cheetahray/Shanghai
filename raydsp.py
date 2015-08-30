a0595fdcc40ce957e7efeab47a4d0791453f3825
cheetahray <pi@raspberrypi.(none)>
2015-08-30 12:23:54 +0000

    Finally I can close the pyo gui

--- raydsp.py
+++ raydsp.py
@@ -1,23 +1,21 @@
 from pyo import *
+import time
+from threading import Timer
+import pyautogui
+
+def print_time():
+    global s
+    s.gui()
+
+def play_wav(raywav):
+    a.setPath(raywav)
+    b.setInput(a) 
+    threading.Timer(0, print_time, ()).start()
+    time.sleep(10)  # sleep while time-delay events execute
+    pyautogui.hotkey('ctrl', 'q')
 
 s = Server(audio="jack").boot()
 s.start()
-t = SndTable("/home/pi/Shanghai/wav/Strat F- 52.wav")
-sf = Looper(t, dur=t.getDur()*2, xfade=0, mul=0.5)
-rev = STRev(sf, inpos=0.25, revtime=2, cutoff=5000, bal=0.25, roomSize=1).out()
-
-#s = Server(audio="jack",duplex=0).boot()
-
-#a = SfPlayer("/home/pi/Shanghai/wav/Strat F- 52.wav", loop=True, mul=0.25).mix(2).out()
-
-#b1 = Allpass(a, delay=[.0204,.02011], feedback=0.35)
-#b2 = Allpass(b1, delay=[.06653,.06641], feedback=0.41)
-#b3 = Allpass(b2, delay=[.035007,.03504], feedback=0.5)
-#b4 = Allpass(b3, delay=[.023021 ,.022987], feedback=0.65)
-
-#c1 = Tone(b1, 5000, mul=0.2).out()
-#c2 = Tone(b2, 3000, mul=0.2).out()
-#c3 = Tone(b3, 1500, mul=0.2).out()
-#c4 = Tone(b4, 500, mul=0.2).out()
-
-s.gui(locals())
+a = SfPlayer("/home/pi/Shanghai/wav/Strat F- 52.wav", loop=False, mul=.4)
+b = Freeverb(a, size=[.79,.8], damp=.9, bal=.3).out()
+play_wav("/home/pi/Shanghai/wav/Strat F- 82.wav")
