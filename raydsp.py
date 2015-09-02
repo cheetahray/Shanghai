62ccd1ff726a7d2a14ae3e5879d96d6e3eb0e158
cheetahray <pi@raspberrypi.(none)>
2015-09-02 13:32:53 +0000

    Tomorrow ready to setamp

--- raydsp.py
+++ raydsp.py
@@ -7,9 +7,6 @@ import math
 s = Server(audio="jack",duplex=0).boot()
 s.start()
 
-def assign():
-    mm.setAmp(vin=0, vout=0, amp=10)
-
 CHUNK = 8192
 pyaud = pyaudio.PyAudio()
 lastfeq = 0
@@ -38,12 +35,12 @@ while True:
         a = SfPlayer(raystr, loop=False, mul=.4)
         mm = Mixer()
         mm.addInput(0,a)
+        mm.setAmp(vin=0, vout=0, amp=10)
         b = WGVerb(mm[0], feedback=0.9, bal=1).out()
-        pat = Pattern(function=assign).play()
+        #pat = Pattern(function=assign,time=0).play()
         #a.setPath(raystr)
         #b.setInput(a)
         #b.out()
-        #pat.play() 
         lastfeq = rayfeq
         #time.sleep(6)
         stream.start_stream()
@@ -51,5 +48,6 @@ while True:
         lastfeq = 0
 
 pyaud.terminate()
+
 
 
