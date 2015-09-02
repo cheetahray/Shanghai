914b8a70bd04d34e14ea6a1aca5431a6bfc9dc04
cheetahray <pi@raspberrypi.(none)>
2015-09-02 09:01:13 +0000

    Got to reinstance pyo every time

--- raydsp.py
+++ raydsp.py
@@ -7,17 +7,9 @@ import math
 s = Server(audio="jack").boot()
 s.start()
 
-a = SfPlayer("/home/pi/Shanghai/wav/52.wav", loop=False, mul=.4)
-mm = Mixer()
-mm.addInput(0,a)
-b = Freeverb(mm[0], size=[.79,.8], damp=.9).out()
-
-# Dynamic assignation of inputs to outputs with random amplitude
 def assign():
-    #global mm
-    mm.setAmp(vin=0, vout=0, amp=10)
+    mm.setAmp(vin=0, vout=0, amp=5)
 
-pat = Pattern(function=assign)
 CHUNK = 8192
 pyaud = pyaudio.PyAudio()
 lastfeq = 0
@@ -34,10 +26,8 @@ stream = pyaud.open(
     )
     
 while True:
-    # Read raw microphone data
-    rawsamps = stream.read(CHUNK)
-    # Convert raw data to NumPy array
-    samps = numpy.fromstring(rawsamps, dtype=numpy.int16)
+    rawsamps = stream.read(CHUNK) # Read raw microphone data
+    samps = numpy.fromstring(rawsamps, dtype=numpy.int16) # Convert raw data to NumPy array
     rayfeq = analyse.musical_detect_pitch(samps)
     #print (analyse.loudness(samps), rayfeq)
     if rayfeq > 0 and math.fabs(rayfeq-lastfeq) > 2:
@@ -45,16 +35,20 @@ while True:
         print (analyse.loudness(samps), rayfeq)
         raystr = "/home/pi/Shanghai/wav/" + str(int(round(rayfeq))) + ".wav"
         print(raystr)
-        a.setPath(raystr) #a = SfPlayer(raystr, loop=False, mul=.4)
+        a = SfPlayer(raystr, loop=False, mul=.4)
+        mm = Mixer()
+        mm.addInput(0,a)
+        b = Freeverb(mm[0], size=[.79,.8], damp=.9).out()
+        pat = Pattern(function=assign).play()
+        #a.setPath(raystr)
         #b.setInput(a)
         #b.out()
-        pat.play() #pat = Pattern(function=assign
+        #pat.play() 
         lastfeq = rayfeq
-        #time.sleep(5)
+        time.sleep(5)
         stream.start_stream()
     else:
         lastfeq = 0
 
 pyaud.terminate()
-
 
