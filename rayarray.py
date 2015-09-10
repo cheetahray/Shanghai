c64de20e12e891015448c703d86010e754cfa0f1
cheetahray <pi@raspberrypi.(none)>
2015-09-10 14:44:05 +0000

    factor change to 60

--- rayarray.py
+++ rayarray.py
@@ -1,22 +1,142 @@
-from pyo import *
-import numpy as np
-import math
+#!/usr/bin/env python
+
 from scipy.io import wavfile
+import argparse
+import numpy as np
+import pygame
+import sys
+import warnings
+from pyo import *
 
-s = Server(audio="jack").boot()
-
-fps, sound = wavfile.read("/home/pi/Shanghai/wav/piano/52.wav")
-
-size = len(sound) #512
-t = DataTable(size, init=sound.T.tolist())
-freq = t.getRate()
-b = TableRead(t, freq, loop = True, mul = .00001).out() 
-#t.view()
+def speedx(snd_array, factor):
+    """ Speeds up / slows down a sound, by some factor. """
+    indices = np.round(np.arange(0, len(snd_array), factor))
+    indices = indices[indices < len(snd_array)].astype(int)
+    return snd_array[indices]
 
 
-#size = len(sound) #256
-#arr2 = np.array([[math.sin(2 * math.pi * j / size + math.sin(i/16.0)) for j in range(size)] for i in range(size)])
-#m = NewMatrix(size, size, sound.tolist())
-#m.view()
+def stretch(snd_array, factor, window_size, h):
+    """ Stretches/shortens a sound, by some factor. """
+    phase = np.zeros(window_size)
+    hanning_window = np.hanning(window_size)
+    result = np.zeros(len(snd_array) / factor + window_size)
 
-s.gui(locals())
+    for i in np.arange(0, len(snd_array) - (window_size + h), h*factor):
+        # Two potentially overlapping subarrays
+        a1 = snd_array[i: i + window_size]
+        a2 = snd_array[i + h: i + window_size + h]
+
+        # The spectra of these arrays
+        s1 = np.fft.fft(hanning_window * a1)
+        s2 = np.fft.fft(hanning_window * a2)
+
+        # Rephase all frequencies
+        phase = (phase + np.angle(s2/s1)) % 2*np.pi
+
+        a2_rephased = np.fft.ifft(np.abs(s2)*np.exp(1j*phase))
+        i2 = int(i/factor)
+        result[i2: i2 + window_size] += hanning_window*a2_rephased.real
+
+    # normalize (16bit)
+    result = ((2**(16-4)) * result/result.max())
+
+    return result.astype('int16')
+
+
+def pitchshift(snd_array, n, window_size=2**13, h=2**11):
+    """ Changes the pitch of a sound by ``n`` semitones. """
+    factor = 2**(1.0 * n / 12.0)
+    stretched = stretch(snd_array, 1.0/factor, window_size, h)
+    return speedx(stretched[window_size:], factor)
+
+
+def parse_arguments():
+    description = ('Use your computer keyboard as a "piano"')
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument(
+        '--wav', '-w',
+        metavar='FILE',
+        type=argparse.FileType('r'),
+        default='/home/pi/Shanghai/wav/horn/52.wav',
+        help='WAV file (default: 52.wav)')
+    parser.add_argument(
+        '--keyboard', '-k',
+        metavar='FILE',
+        type=argparse.FileType('r'),
+        default='/home/pi/pianoputer/typewriter.kb',
+        help='keyboard file (default: typewriter.kb)')
+    parser.add_argument(
+        '--verbose', '-v',
+        action='store_true',
+        help='verbose mode')
+
+    return (parser.parse_args(), parser)
+
+def raysound(arr):
+    t = DataTable(size=len(arr), init=arr.T.tolist())
+    return TableRead(t, freq=t.getRate(), loop = False, mul = .001)
+
+def main():
+    s = Server(audio="jack",duplex=0).boot()
+    s.start()
+
+    # Parse command line arguments
+    (args, parser) = parse_arguments()
+
+    # Enable warnings from scipy if requested
+    if not args.verbose:
+        warnings.simplefilter('ignore')
+
+    fps, sound = wavfile.read(args.wav.name)
+
+    tones = range(-5, 5)
+    sys.stdout.write('Transponding sound file... ')
+    sys.stdout.flush()
+    transposed_sounds = [pitchshift(sound, n) for n in tones]
+    print('DONE')
+
+    # So flexible ;)
+    #pygame.mixer.init(fps, -16, 1, 100)
+    # For the focus
+    #screen = pygame.display.set_mode((150, 150))
+
+    keys = args.keyboard.read().split('\n')
+    sounds = map(raysound, transposed_sounds)
+   
+    #key_sound = dict(zip(keys, sounds))
+    #is_playing = {k: False for k in keys}
+
+    raycnt = 0
+    for i in sounds:
+        i.out()
+        print (raycnt) 
+        raycnt = raycnt + 1
+        time.sleep(2)
+
+    #while True:
+        #event = pygame.event.wait()
+
+        #if event.type in (pygame.KEYDOWN, pygame.KEYUP):
+        #    key = pygame.key.name(event.key)
+
+        #if event.type == pygame.KEYDOWN:
+        #    if (key in key_sound.keys()) and (not is_playing[key]):
+        #        key_sound[key].out()
+        #        is_playing[key] = True
+
+        #    elif event.key == pygame.K_ESCAPE:
+        #        pygame.quit()
+        #        raise KeyboardInterrupt
+
+        #elif event.type == pygame.KEYUP and key in key_sound.keys():
+        #    is_playing[key] = False
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Goodbye')
+
+
