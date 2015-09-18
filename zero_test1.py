75e7d54deb4222661382e38b55e22a4367574846
ray <ray@dac.tw>
2015-09-18 23:51:04 +0800

    寫到 a 還算頭痛 後面還有 thread

--- zero_test1.py
+++ zero_test1.py
@@ -4,6 +4,7 @@
 #------------------------------------------------------------------------
 #------------------------------------------------------------------------
 import stepper_class
+import socket
 #------------------------------------------------------------------------
 tp=[]
 tp.append(12800)
@@ -14,7 +15,31 @@ tp.append(64000)
 tp.append(76800)
 tp.append(89600)
 
+UDP_IP = "192.168.11.27"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind(("127.0.0.1", UDP_PORT))
 
+data, addr = sock.recvfrom(1024)
+if data == "tsh":
+    print("壓弦 回 Home")
+    if True: #如果壓弦回 Home 成功
+        sock.sendto(data + "e", (UDP_IP, UDP_PORT))
+elif data == "tph":
+    print("picker 回 Home")
+    if True: #如果 picker 回 Home 成功
+        sock.sendto(data + "e", (UDP_IP, UDP_PORT))
+elif data == "tvh":
+    print("velocity 回 Home")
+    if True: #如果 velocity 回 Home 成功
+        sock.sendto(data + "e", (UDP_IP, UDP_PORT))
+elif data == "tsl":    
+    print("弦轉鬆")
+elif data == "tst":
+    print("弦轉緊")
+elif data == "tss":
+    print("弦轉停")
 
 stepper_class.io1.wiringPiSetupGpio()
 motor1 = stepper_class.stepperdriver(12800,19,26)
