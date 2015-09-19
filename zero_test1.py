584961323f2ae82732947f96ff76749cd7030b4e
ray <ray@dac.tw>
2015-09-19 23:35:31 +0800

    Motor seems to initial for a long time. Comment

--- zero_test1.py
+++ zero_test1.py
@@ -7,49 +7,75 @@ import stepper_class
 import socket
 #------------------------------------------------------------------------
 tp=[]
-tp.append(12800)
-tp.append(25600)
-tp.append(38400)
-tp.append(51200)
-tp.append(64000)
-tp.append(76800)
-tp.append(89600)
+
+#stepper_class.io1.wiringPiSetupGpio()
+#motor1 = stepper_class.stepperdriver(12800,19,26)
+#motor2 = stepper_class.stepperdriver(12800,20,21)
+
+#motor1.gh(-1,20,0.1,6)
+#motor2.gh(1,2,0.1,16)
 
 UDP_IP = "192.168.11.27"
 UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind(("127.0.0.1", UDP_PORT))
+sock.bind(("", UDP_PORT))
 
-data, addr = sock.recvfrom(1024)
-if data == "tsh":
-    print("壓弦 回 Home")
-    if True: #如果壓弦回 Home 成功
-        sock.sendto(data + "e", (UDP_IP, UDP_PORT))
-elif data == "tph":
-    print("picker 回 Home")
-    if True: #如果 picker 回 Home 成功
-        sock.sendto(data + "e", (UDP_IP, UDP_PORT))
-elif data == "tvh":
-    print("velocity 回 Home")
-    if True: #如果 velocity 回 Home 成功
-        sock.sendto(data + "e", (UDP_IP, UDP_PORT))
-elif data == "tsl":    
-    print("弦轉鬆")
-elif data == "tst":
-    print("弦轉緊")
-elif data == "tss":
-    print("弦轉停")
+iftsh = False
+iftph = False
+iftvh = False
+while False == iftsh or False == iftph or False == iftvh:
+    data, addr = sock.recvfrom(1024)
+    if data == "tsh":
+        print("String back home")
+        if True: #if success
+            sock.sendto(data + "e", (UDP_IP, UDP_PORT))
+            iftsh = True
+    elif data == "tph":
+        print("picker back home")
+        if True: #if success
+            sock.sendto(data + "e", (UDP_IP, UDP_PORT))
+            iftph = True 
+    elif data == "tvh":
+        print("velocity back home")
+        if True: #if success
+            sock.sendto(data + "e", (UDP_IP, UDP_PORT))
+            iftvh = True
 
-stepper_class.io1.wiringPiSetupGpio()
-motor1 = stepper_class.stepperdriver(12800,19,26)
-motor2 = stepper_class.stepperdriver(12800,20,21)
+pickspeed = 0
+pickvelocity = 0
+ifpickclock = True
+motorspeed = 0
+while True:
+    data, addr = sock.recvfrom(1024)
+    if data[0] == 't':
+        if data[1:] == "sl":
+            print("turn string loose")
+        elif data[1:] == "st":
+            print("turn string tight")
+        elif data[1:] == "ss":
+            print("stop turn string")
+    elif data[0] == 'a':
+        if data[1] == 'v':
+            pickvelocity = int(data[2:])
+        elif data[1] == 'c':
+            ifpickclock = True
+            pickspeed = int(data[2:])
+        elif data[1] == 'w':
+            ifpickclock = False
+            pickspeed = int(data[2:])
+        print("use picker speed = {0}, picker velocity = {1}, is picker clockwise? {2}".format(pickspeed,pickvelocity,ifpickclock))
+    elif data[0] == 'm':
+        if data[1] == 'r':
+            tp.insert( int(data[2:]), "Remember pitch {0} position".format( int(data[2:]) ) )
+            print( tp[ int( data[2:] ) ] )
+        else:
+            vindex = data.index('v')
+            motorspeed = int(data[vindex:])
+            print("Use {0} speed to get pitch {1} position {2}".format( motorspeed, int(data[2:vindex]), tp[ int(data[2:vindex]) ] ) )
 
-motor1.gh(-1,20,0.1,6)
-motor2.gh(1,2,0.1,16)
-
-motor1.moveto(tp[1],300,20,20)
-stepper_class.io1.delayMicroseconds(500000)
+#motor1.moveto(tp[1],300,20,20)
+#stepper_class.io1.delayMicroseconds(500000)
 #motor1.moveto(tp[3],300,20,20)
 #stepper_class.io1.delayMicroseconds(500000)
 #motor1.moveto(tp[5],300,20,20)
@@ -62,4 +88,4 @@ stepper_class.io1.delayMicroseconds(500000)
 #stepper_class.io1.delayMicroseconds(500000)
 #motor1.moveto(tp[0],300,20,20)
 #stepper_class.io1.delayMicroseconds(500000)
-motor1.moveto(0,300,20,20)
+#motor1.moveto(0,300,20,20)
