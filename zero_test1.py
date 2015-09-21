bcb3e269fb64dc8287393e97214948c01b207703
ray <ray@dac.tw>
2015-09-21 14:03:35 +0800

    Add some comment for Albert

--- zero_test1.py
+++ zero_test1.py
@@ -25,18 +25,18 @@ while False == iftsh or False == iftph or False == iftvh:
 #while False:
     data, addr = sock.recvfrom(1024)
     if data == "tsh":
-        print("String back home") #change here
-        if True: #change here
+        print("String back home") #tsh change here
+        if True: #tshe change here
             sock.sendto(data + "e", (UDP_IP, UDP_PORT))
             iftsh = True
     elif data == "tph":
-        print("picker back home") #change here
-        if True: #change here
+        print("picker back home") #tph change here
+        if True: #tphe change here
             sock.sendto(data + "e", (UDP_IP, UDP_PORT))
             iftph = True 
     elif data == "tvh":
-        print("velocity back home") #change here
-        if True: #change here
+        print("velocity back home") #tvh change here
+        if True: #tvhe change here
             sock.sendto(data + "e", (UDP_IP, UDP_PORT))
             iftvh = True
 
@@ -49,11 +49,11 @@ while True:
     #print (data)
     if data[0] == 't':
         if data[1:] == "sl":
-            print("turn string loose") #change here
+            print("turn string loose") #tsl change here
         elif data[1:] == "st": 
-            print("turn string tight") #change here
+            print("turn string tight") #tst change here
         elif data[1:] == "ss":
-            print("stop turn string") #change here
+            print("stop turn string") #tss change here
     elif data[0] == 'a':
         if data[1] == 'v':
             pickvelocity = int(data[2:])
@@ -63,22 +63,22 @@ while True:
         elif data[1] == 'w':
             ifpickclock = False
             pickspeed = int(data[2:])
-        print("use picker speed = {0}, picker velocity = {1}, is picker clockwise? {2}".format(pickspeed,pickvelocity,ifpickclock)) #change here
+        print("use picker speed = {0}, picker velocity = {1}, is picker clockwise? {2}".format(pickspeed,pickvelocity,ifpickclock)) #av127, ac127, aw127 change here
     elif data[0] == 'm':
         if data[1] == 'r':
             tpindex = int(data[2:]) 
-            tp.append("Remember pitch {0} position".format( tpindex ) ) #change here
-            print( tp[ len(tp)-1 ] ) #change here
+            tp.append("Remember pitch {0} position".format( tpindex ) ) #mr1 change here
+            print( tp[ len(tp)-1 ] ) #mr1 change here
         elif data[1] == 'h':
-            print("left hand move high") #change here
+            print("left hand move high") #mh change here
         elif data[1] == 'l':
-            print("left hand move low") #change here
+            print("left hand move low") #ml change here
         elif data[1] == 's':
-            print("left hand stop move") #change here
+            print("left hand stop move") #ms change here
         else:
             vindex = data.index('v') 
             motorspeed = int(data[vindex+1:]) 
-            print("Use {0} motor speed to get pitch {1} position from tp[ int(data[1:vindex]) - 1 ]".format( motorspeed, int(data[1:vindex]) ) ) #change here
+            print("Use {0} motor speed to get pitch {1} position from tp[ int(data[1:vindex]) - 1 ]".format( motorspeed, int(data[1:vindex]) ) ) #m1v127 change here
 
 #motor1.gh(-1,20,0.1,6)
 #motor2.gh(1,2,0.1,16)
