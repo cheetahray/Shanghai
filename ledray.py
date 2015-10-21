ea5d01f8befbc673054c768a6678b525b023deb6
cheetahray <pi@raspberrypi.(none)>
2015-10-21 12:57:17 +0000

    Change time sleep

--- ledray.py
+++ ledray.py
@@ -16,29 +16,31 @@ from strip_animations import *
 import bibliopixel.colors as colors
 anim = ColorWipe(led)
 
+import math
+
 def rayanim(r,g,b,bright,animpos,animtime):
     anim.brightcolor( bright, (b,r,g) )
-    if anim.lastIndex+1 > animpos :
+    diff = anim.lastIndex + 1 - animpos
+    animtime = animtime / math.fabs(diff)
+    if diff > 0 :
         for i in range(anim.lastIndex,animpos-1,-1):
             anim.gogo(rayIndex = i)
-            anim.lastIndex = i
             anim.run(threaded = True, joinThread = False)
             time.sleep(animtime)
-    elif anim.lastIndex+1 < animpos:
+        anim.lastIndex = animpos
+        time.sleep(0.2)
+    elif diff < 0:
         for i in range(anim.lastIndex,animpos+1,1):
             anim.gogo(rayIndex = i)
-            anim.lastIndex = i
             anim.run(threaded = True, joinThread = False)
             time.sleep(animtime)
-
+        anim.lastIndex = animpos
+        time.sleep(0.2)
 try:
     #run the animation
-    rayanim(255,0,0,255,20,0.5)
-    time.sleep(0.5)
-    rayanim(0,255,0,150,5,0.5)
-    time.sleep(0.5)
-    rayanim(0,0,255,75,15,0.5)
-    time.sleep(0.5)
+    rayanim(255,0,0,255,20,3.0)
+    rayanim(0,255,0,150,5,1.5)
+    rayanim(0,0,255,75,15,0.7)
     rayanim(255,255,255,30,0,0.5)
 except KeyboardInterrupt:
     #Ctrl+C will exit the animation and turn the LEDs offs
