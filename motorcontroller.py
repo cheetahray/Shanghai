b45c57d7bb58bf25aa226fa15cde6f52c4a69de3
cheetahray <pi@raspberrypi.(none)>
2015-10-23 05:45:52 +0000

    Add os fork into motor

--- motorcontroller.py
+++ motorcontroller.py
@@ -6,6 +6,8 @@
 import stepper_class
 import socket
 #------------------------------------------------------------------------
+import os
+
 tp=[]
 
 stepper_class.io1.wiringPiSetupGpio()
@@ -49,11 +51,20 @@ while True:
     #print (data)
     if data[0] == 't':
         if data[1:] == "sl":
-            print("turn string loose") #tsl change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print("turn string loose") #tsl change here
+                os._exit(0)
         elif data[1:] == "st": 
-            print("turn string tight") #tst change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print("turn string tight") #tst change here
+                os._exit(0)
         elif data[1:] == "ss":
-            print("stop turn string") #tss change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print("stop turn string") #tss change here
+                os._exit(0)
     elif data[0] == 'a':
         if data[1] == 'v':
             pickvelocity = int(data[2:])
@@ -63,22 +74,40 @@ while True:
         elif data[1] == 'w':
             ifpickclock = False
             pickspeed = int(data[2:])
-        print("use picker speed = {0}, picker velocity = {1}, is picker clockwise? {2}".format(pickspeed,pickvelocity,ifpickclock)) #av127, ac127, aw127 change here
+        child_pid = os.fork()
+        if child_pid == 0:
+            print("use picker speed = {0}, picker velocity = {1}, is picker clockwise? {2}".format(pickspeed,pickvelocity,ifpickclock)) #av127, ac127, aw127 change here
+            os._exit(0)
     elif data[0] == 'm':
         if data[1] == 'r':
             tpindex = int(data[2:]) 
             tp.append("Remember pitch {0} position".format( tpindex ) ) #mr1 change here
-            print( tp[ len(tp)-1 ] ) #mr1 change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print( tp[ len(tp)-1 ] ) #mr1 change here
+                os._exit(0)
         elif data[1] == 'h':
-            print("left hand move high") #mh change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print("left hand move high") #mh change here
+                os._exit(0)
         elif data[1] == 'l':
-            print("left hand move low") #ml change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print("left hand move low") #ml change here
+                os._exit(0)
         elif data[1] == 's':
-            print("left hand stop move") #ms change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print("left hand stop move") #ms change here
+                os._exit(0)
         else:
             vindex = data.index('v') 
             motorspeed = int(data[vindex+1:]) 
-            print("Use {0} motor speed to get pitch {1} position from tp[ int(data[1:vindex]) - 1 ]".format( motorspeed, int(data[1:vindex]) ) ) #m1v127 change here
+            child_pid = os.fork()
+            if child_pid == 0:
+                print("Use {0} motor speed to get pitch {1} position from tp[ int(data[1:vindex]) - 1 ]".format( motorspeed, int(data[1:vindex]) ) ) #m1v127 change here
+                os._exit(0)
 
 #motor1.gh(-1,20,0.1,6)
 #motor2.gh(1,2,0.1,16)
