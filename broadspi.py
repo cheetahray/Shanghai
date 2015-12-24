912c09d453cf1350eea048707698e9f76d65e730
ray <ray@dac.tw>
2015-12-24 14:48:09 +0800

    11  Thread 炸 SPI 訊號

---
+++ broadspi.py
@@ -0,0 +1,254 @@
+import socket  
+import thread
+import time
+
+def Threadfun1(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun2(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun3(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun4(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun5(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun6(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun7(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun8(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun9(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+def Threadfun10(string1, string2, string3, string4, string5, string6, *args):
+    global data
+    global sleeptime
+    s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s5 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s6 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while(True):
+        s1.sendto(data, (string1 ,6454) )
+        s2.sendto(data, (string2, 6454) )
+        s3.sendto(data, (string3, 6454) )
+        s4.sendto(data, (string4, 6454) )
+        s5.sendto(data, (string5, 6454) )
+        s6.sendto(data, (string6, 6454) )
+        time.sleep(sleeptime)
+
+address = ('0.0.0.0', 6454)  
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+s.bind(address)  
+
+s61 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+s62 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+s63 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+s64 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+s65 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+s66 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+  
+data = ""
+sleeptime = 0.001
+
+i = 1
+thread.start_new_thread(Threadfun1, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 7
+thread.start_new_thread(Threadfun2, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 13
+thread.start_new_thread(Threadfun3, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 19
+thread.start_new_thread(Threadfun4, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 25
+thread.start_new_thread(Threadfun5, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 31
+thread.start_new_thread(Threadfun6, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 37
+thread.start_new_thread(Threadfun7, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 43
+thread.start_new_thread(Threadfun8, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 49
+thread.start_new_thread(Threadfun9, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+i = 55
+thread.start_new_thread(Threadfun10, ("192.168.12." + str(i), "192.168.12." + str(i+1), "192.168.12." + str(i+2), 
+                                     "192.168.12." + str(i+3), "192.168.12." + str(i+4), "192.168.12." + str(i+5)
+                                         ) )
+  
+while True:  
+    data, addr = s.recvfrom(8192)  
+    #if not data:  
+    #    print "client has exist"  
+    #    break  
+	  #print "received:", data, "from", addr
+    s61.sendto(data, ("192.168.12.61",6454) )
+    s62.sendto(data, ("192.168.12.62",6454) )
+    s63.sendto(data, ("192.168.12.63",6454) )
+    s64.sendto(data, ("192.168.12.64",6454) )
+    s65.sendto(data, ("192.168.12.65",6454) )
+    s66.sendto(data, ("192.168.12.66",6454) )
+    time.sleep(sleeptime)
+    
+s.close()  
