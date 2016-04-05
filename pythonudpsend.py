f8f1af70e4cf34c292010b5f67c969d04a6767f4
ray <ray@dac.tw>
2016-04-05 16:52:39 +0800

    192.168.12.202 1234

--- pythonudpsend.py
+++ pythonudpsend.py
@@ -4,7 +4,7 @@ import sys
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('localhost', 12345)
+server_address = ('192.168.12.202', 1234)
 message = 'This is the message.  It will be repeated.'
 
 try:
