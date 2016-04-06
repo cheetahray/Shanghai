864b8f1e7d1fb73a25de022258607ee49fc0cca6
cheetahray <ray@dac.tw>
2016-04-06 11:53:52 +0800

    Change to 0.0.0.0

--- pythonudpsend.py
+++ pythonudpsend.py
@@ -1,18 +1,19 @@
 import socket
 import sys
-
+import time
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('192.168.12.202', 1234)
-message = 'This is the message.  It will be repeated.'
+server_address = ('172.1.0.3', 12345)
+message = 'Yes or No?'
 
 try:
 
-    # Send data
-    print >>sys.stderr, 'sending "%s"' % message
-    sent = sock.sendto(message, server_address)
-
+    while True:
+        # Send data
+        print >>sys.stderr, 'sending "%s"' % message
+        sent = sock.sendto(message, server_address)
+        time.sleep(1)
 finally:
     print >>sys.stderr, 'closing socket'
     sock.close()
