91c2fabb7429c37ee56a3d9f0b5541bbf3abd7b1
ray <ray@dac.tw>
2016-04-06 20:24:17 +0800

    192.168.4.1

--- pythonudpsend.py
+++ pythonudpsend.py
@@ -4,15 +4,16 @@ import time
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('172.1.0.3', 12345)
+server_address = ('192.168.4.1', 8888)
 message = 'Yes or No?'
-
+cnt = 0
 try:
 
     while True:
         # Send data
-        print >>sys.stderr, 'sending "%s"' % message
-        sent = sock.sendto(message, server_address)
+        print >>sys.stderr, 'sending "%d"' % cnt
+        sent = sock.sendto(str(cnt), server_address)
+        cnt = cnt + 1
         time.sleep(1)
 finally:
     print >>sys.stderr, 'closing socket'
