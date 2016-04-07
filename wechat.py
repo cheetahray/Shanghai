45facf012ed54a5a116ca01f7031c9cd6c8a4544
ray <ray@dac.tw>
2016-04-07 15:37:40 +0800

    str for int

--- wechat.py
+++ wechat.py
@@ -391,7 +391,7 @@ def change3(isslider):
     if True == isslider:
         thethree[howmanyCM] = random.sample([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66 ],  3)
         howmanyCM += 1
-        raysendto("CM" + howmanyCM + "F" + thethree[0] + "S" + thethree[1] + "T" + thethree[2], "202", 12345 )
+        raysendto("CM" + str(howmanyCM) + "F" + str(thethree[0]) + "S" + str(thethree[1]) + "T" + str(thethree[2]), "202", 12345 )
         time.sleep(60) # maybe recvfrom another udp string to start it, now it's my control
         thread.start_new_thread(play_midi,())       
     else:
