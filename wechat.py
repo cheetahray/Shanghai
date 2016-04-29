dcaf28798e931c8149e336077044326f32613a8e
ray <ray@dac.tw>
2016-04-29 18:23:49 +0800

    Why

--- wechat.py
+++ wechat.py
@@ -314,7 +314,7 @@ def play_midi():
                     raysendto("128 " + str(message.note) + " " + str(message.velocity) + " " + str(pickidx[8][message.note]) , str(pickidx[8][message.note]) )
                     del pickidx[8][message.note]
     raysendto("EndofGame", "202", 12345 )
-    time.sleep(5);
+    time.sleep(6);
     for i in range(66,33,-1):
         raysendto("225 1", str(i) )
         raysendto("225 1", str(67-i) )
@@ -329,7 +329,7 @@ def play_midi():
         if 1 == BT[i]:
             raysendto("144 28 1 " + str(i) , str(i) )
         time.sleep(0.02)
-    time.sleep(5);
+    time.sleep(3);
     for i in range(1,67):
         if 1 == ST[i]:
             raysendto("144 60 0 " + str(i), str(i))
@@ -340,7 +340,7 @@ def play_midi():
         if 1 == BT[i]:
             raysendto("144 28 0 " + str(i), str(i))
         time.sleep(0.02)
-    time.sleep(5);
+    time.sleep(3);
     for i in range(1,67):
         raysendto("249 2" , str(i))
         time.sleep(0.01)
