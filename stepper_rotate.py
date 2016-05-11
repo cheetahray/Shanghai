27289f9b121d57f123bc4dfb7df3063ef4ea1809
cheetahray <ray@dac.tw>
2016-05-11 14:34:31 +0800

    Add rotate

---
+++ stepper_rotate.py
@@ -0,0 +1,25 @@
+#------------------------------------------------------------------------
+#Code Written by: Albert Lin from Noise Kitchen
+#Current Project: noisekitchen.tw
+#------------------------------------------------------------------------
+
+#Step 1: Import necessary libraries 
+#------------------------------------------------------------------------
+import sys
+import motor_lib
+#------------------------------------------------------------------------
+
+#Step 2: read the direction and number of steps; if steps are 0 exit 
+#------------------------------------------------------------------------
+try: 
+    direction = sys.argv[1]
+    laps = float(sys.argv[2])
+    rpm= float(sys.argv[3])
+    at= float(sys.argv[4])
+    dt= float(sys.argv[5])
+except:
+    laps = 0
+
+motor_lib.io1.wiringPiSetupGpio()
+motor1 = motor_lib.stepperdriver(6400,5,6)
+motor1.rotate(direction, laps,rpm,at,dt)
