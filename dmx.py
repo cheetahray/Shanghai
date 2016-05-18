77a0c51e0f7cd7c3caeb70739e07c8e357d81c55
cheetahray <ray@dac.tw>
2016-05-19 02:13:26 +0800

    God

--- dmx.py
+++ dmx.py
@@ -19,13 +19,13 @@ chan = [ 0  , 0  , 0  , 0  ,
          255, 255, 255, 255 ]
 jj = 0
 while True:
-    for ii in range(0,64,4):         
+    for ii in range(1,65,4):         
         mydmx.setChannel(ii, chan[jj]) # set DMX channel 1 to full
         mydmx.setChannel(ii+1, chan[jj+1]) # set DMX channel 2 to 128
         mydmx.setChannel(ii+2, chan[jj+2]) # set DMX channel 3 to 0
         mydmx.setChannel(ii+3, chan[jj+3])
     mydmx.render()
-    if jj < 64:
+    if jj < 60:
     	jj += 4
     else:
       jj = 0
