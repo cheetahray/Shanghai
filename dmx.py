840fdd787937ee9e813e872d07ab2ddb2ac8bfa8
cheetahray <ray@dac.tw>
2016-05-20 02:52:05 +0800

    Dmx ok

--- dmx.py
+++ dmx.py
@@ -1,37 +1,40 @@
 import pysimpledmx
 import time
 mydmx = pysimpledmx.DMXConnection("/dev/ttyUSB0")
-chan = [ 0  , 0  , 0  , 0  ,
-         0  , 0  , 0  , 255,
-         0  , 0  , 255, 0  ,
-         0  , 0  , 255, 255,
-         0  , 255, 0  , 0  ,
-         0  , 255, 0  , 255,
-         0  , 255, 255, 0  ,
-         0  , 255, 255, 255,
-         255, 0  , 0  , 0  ,
-         255, 0  , 0  , 255,
-         255, 0  , 255, 0  ,
-         255, 0  , 255, 255,
-         255, 255, 0  , 0  ,
-         255, 255, 0  , 255,
-         255, 255, 255, 0  ,
-         255, 255, 255, 255 ]
+'''
+chan = [ 0, 0, 0, 0,
+         0, 0, 0, 1,
+         0, 0, 1, 0,
+         0, 0, 1, 1,
+         0, 1, 0, 0,
+         0, 1, 0, 1,
+         0, 1, 1, 0,
+         0, 1, 1, 1,
+         1, 0, 0, 0,
+         1, 0, 0, 1,
+         1, 0, 1, 0,
+         1, 0, 1, 1,
+         1, 1, 0, 0,
+         1, 1, 0, 1,
+         1, 1, 1, 0,
+         1, 1, 1, 1 ]
+'''
+chan = [ 
+         0, 0, 0, 1,
+         0, 0, 1, 0,
+         0, 1, 0, 0,
+         1, 0, 0, 0
+       ]
 jj = 0
 while True:
-    for ii in range(2,66,4):         
-        mydmx.setChannel(ii, chan[jj]) # set DMX channel 1 to full
-        print ii, chan[jj]
-        mydmx.setChannel(ii+1, chan[jj+1]) # set DMX channel 2 to 128
-        print ii+1, chan[jj+1]
-        mydmx.setChannel(ii+2, chan[jj+2]) # set DMX channel 3 to 0
-        print ii+2, chan[jj+2]
-        mydmx.setChannel(ii+3, chan[jj+3])    
-        print ii+3, chan[jj+3]
-    print jj
-    mydmx.render()
-    if jj >= 60:
-        jj = 0
-    else:
-        jj += 4
-    time.sleep(10)
+    for ii in range(0,16,4):         
+        mydmx.setChannel(1, chan[ii]) # set DMX channel 1 to full
+        print chan[ii]
+        mydmx.setChannel(2, chan[ii+1]) # set DMX channel 2 to 128
+        print chan[ii+1]
+        mydmx.setChannel(3, chan[ii+2]) # set DMX channel 3 to 0
+        print chan[ii+2]
+        mydmx.setChannel(4, chan[ii+3])    
+        print chan[ii+3]
+        mydmx.render()
+        time.sleep(5)
