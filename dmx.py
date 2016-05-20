55dbf3305c5a8d162901f41ed2db0aa197819001
cheetahray <ray608@gmail.com>
2016-05-20 18:23:23 +0800

    最終版

--- dmx.py
+++ dmx.py
@@ -1,7 +1,7 @@
 import pysimpledmx
 import time
 mydmx = pysimpledmx.DMXConnection("/dev/ttyUSB0")
-'''
+
 chan = [ 0, 0, 0, 0,
          0, 0, 0, 1,
          0, 0, 1, 0,
@@ -25,17 +25,18 @@ chan = [
          0, 1, 0, 0,
          1, 0, 0, 0
        ]
+'''
 jj = 0
 while True:
-    for ii in range(0,16,4):         
+    for ii in range(0,64,4):         
         mydmx.setChannel(2, chan[ii]) # set DMX channel 1 to full
-        print chan[ii]
+        #print chan[ii]
         mydmx.setChannel(3, chan[ii+1]) # set DMX channel 2 to 128
-        print chan[ii+1]
+        #print chan[ii+1]
         mydmx.setChannel(4, chan[ii+2]) # set DMX channel 3 to 0
-        print chan[ii+2]
+        #print chan[ii+2]
         mydmx.setChannel(5, chan[ii+3])    
-        print chan[ii+3]
+        #print chan[ii+3]
         mydmx.render()
-        time.sleep(1.5)
-        print "Fa"
+        time.sleep(5)
+        #print "Fa"
