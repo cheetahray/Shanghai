40dd2b2f8b4e5363a452aac9c739a33f657fc5be
System Administrator <root@Mac-mini.local>
2016-09-24 15:40:07 +0800

    Change Autumn

--- ShanghaiTest.py
+++ ShanghaiTest.py
@@ -312,14 +312,14 @@ try:
     #port.flushInput()
     #port.flushOutput()
 
-    whoami = "68"
+    whoami = "51"
     #Register the door bl button GPIO input call back function
     #port.sendto(pack('BBB', 253, int(whoami),100), ("192.168.12." + whoami, 5005) )
     time.sleep(0.01)
     port.sendto(pack('BB', 225, 0), ("192.168.12." + whoami, 5005) )
     time.sleep(0.01)
     whattype = 'A'
-    if True:
+    if False:
         for i in range(1,7):
             if whattype == 'S':
                 port.sendto(pack('BBB', 144, 79, 127), ("192.168.12." + whoami, 5005))
