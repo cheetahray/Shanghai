e8a29cb1b9c56f58c33e0a67b0032994af0ae5e7
cheetahray <ray@dac.tw>
2016-09-25 18:48:46 +0800

    Update

--- ChineseMusic.py
+++ ChineseMusic.py
@@ -134,13 +134,11 @@ def lightinout(lightin):
         print "67"
         subprocess.call('./closeart.sh', shell=True)
         for i in range(1,67):
-            port2.sendto( pack('BB', 225, 0), ("%s%d" % ("192.168.12.", i), 5005) )
-            #port.sendto("225 0", ("%s%d" % ("192.168.12.", 67-i), 5005) )
+            threading.Timer(0.01*i, port2.sendto, [pack('BB', 225, 0), ("%s%d" % ("192.168.12.", i), 5005) ]).start()
     elif nowisin != 0 and lightin == 0:
         print "67"
-        for i in range(66,0,-1):
-            port2.sendto( pack('BB', 225, 1), ("%s%d" % ("192.168.12.", i), 5005) )
-            #port.sendto("225 1", ("%s%d" % ("192.168.12.", 67-i), 5005) )
+        for i in range(1,67):
+            threading.Timer(0.01*i, port2.sendto, [pack('BB', 225, 1), ("%s%d" % ("192.168.12.", i), 5005) ]).start()
     nowisin = lightin
 
 def BoomBoom(rayrandom, myType):
