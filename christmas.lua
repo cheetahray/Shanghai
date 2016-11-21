30f855939cc875d88eff0290bc76e72db93a07d1
ray <ray@dac.tw>
2016-11-21 20:11:14 +0800

    Now we got to test the two nodemcu connect with each other

--- christmas.lua
+++ christmas.lua
@@ -1,4 +1,54 @@
 wifiFile = "wifi.txt"
+-- 30s time out for a inactive client
+sv = net.createServer(net.UDP, 30)
+
+_1stpin = 1
+_2ndpin = 2
+pwm.setup(_1stpin, 1000, 0)
+pwm.setup(_2ndpin, 1000, 0)
+--pwm.setup(3, 500, 0)
+pwm.start(_1stpin)
+pwm.start(_2ndpin)
+--pwm.start(3)
+myport = 8008
+
+function opennet()
+    -- server listens on 80, if data received, print data to console and send "hello world" back to caller
+    sv:listen(myport, function(c)
+        c:on("receive", function(c, pl) 
+            checkitout(pl)
+        end)
+        -- c:send("hello world")
+    end)
+end
+
+function checkitout(cc)
+    print( cc )
+    if string.len(cc) > 1 then
+        count = 1
+        ssid = ""
+        pwd = ""
+        for word in string.gmatch(cc, "%a+") do 
+            if count == 1 then
+                ssid = word
+                count = count + 1
+            else
+                pwd = word
+            end
+            print(word) 
+        end
+        writeWifi(ssid, pwd)
+    else
+        -- 7th bit is 1 ?
+        -- print( bit.isset(c, 7) )
+        if bit.isset(cc, 7) == 1 then
+            pwm.setduty(_1stpin, bit.lshift(bit.band(cc, 127), 3) )
+        else
+            pwm.setduty(_2ndpin, bit.lshift(bit.band(cc, 127), 3) )
+        end
+        print (bit.lshift(bit.band(cc, 127), 3))
+    end
+end
 
 function writeWifi(ssid,password) 
     -- open 'init.lua' in 'a+' mode
@@ -11,22 +61,20 @@ function writeWifi(ssid,password)
 end 
 
 if not file.exists(wifiFile) then
-    writeWifi("bellclass","NoiseKitchen")
+    -- writeWifi("bellclass","NoiseKitchen")
+    writeWifi("DAC-2F(rear)","yellowsub")
 end
-
--- open 'init.lua', print the first line.
---[[
-if file.open(wifiFile, "r") then
-    cfg.ssid = file.readline()
-    cfg.ssid = string.sub(cfg.ssid, 0, string.len(cfg.ssid) - 1 )
-    cfg.pwd = file.readline()
-    cfg.pwd = string.sub(cfg.pwd, 0, string.len(cfg.pwd) - 1 )
-    file.close()
-end
---]]
 
 wifi.setmode(wifi.STATIONAP)
-wifi.sta.config("DAC-2F(rear)","yellowsub")
+if file.open(wifiFile, "r") then
+    ssid = file.readline()
+    ssid = string.sub(ssid, 0, string.len(ssid) - 1 )
+    pwd = file.readline()
+    pwd = string.sub(pwd, 0, string.len(pwd) - 1 )
+    wifi.sta.config(ssid, pwd)
+    file.close()
+end
+
 foundap = 1
 
 cfg={}
@@ -50,15 +98,8 @@ wifi.sta.eventMonReg(wifi.STA_APNOTFOUND, function()
         print(cfg.ssid)
         wifi.ap.config(cfg)
         wifi.sta.eventMonStop()
-         -- 30s time out for a inactive client
-        sv = net.createServer(net.UDP, 30)
-        -- server listens on 80, if data received, print data to console and send "hello world" back to caller
-        sv:listen(80, function(c)
-            c:on("receive", function(c, pl) 
-                print(pl)
-            end)
-            c:send("hello world")
-        end)
+        
+        opennet()
     end 
 end)
 
@@ -91,17 +132,8 @@ wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
     wifi.sta.eventMonStop()
     wifi.eventmon.unregister(wifi.eventmon.STA_CONNECTED)
     wifi.eventmon.unregister(wifi.eventmon.STA_GOT_IP)
-    srv = net.createConnection(net.UDP, 0)
-    srv:on("receive", function(sck, c) 
-        print(c) 
-    end)
-    --[[
-    srv:connect(80,"192.168.0.66")
-    srv:on("connection", function(sck, c)
-        -- Wait for connection before sending.
-        sck:send("GET / HTTP/1.1\r\nHost: 192.168.0.66\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n")
-    end)
-    --]]
+
+    opennet()
 end)
 
 wifi.eventmon.register(wifi.eventmon.AP_STACONNECTED, function(T) 
@@ -110,4 +142,4 @@ wifi.eventmon.register(wifi.eventmon.AP_STACONNECTED, function(T)
 end)
 
 --unregister callback
---wifi.sta.eventMonReg(wifi.STA_IDLE)
+wifi.sta.eventMonReg(wifi.STA_IDLE)
