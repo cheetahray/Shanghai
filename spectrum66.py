45ffcefdd9d6152b8f4bb18eb48727ddbf0d7079
cheetahray <ray@dac.tw>
2016-11-30 14:42:59 +0800

    Change to 4 minutes can not input another midi songs

--- spectrum66.py
+++ spectrum66.py
@@ -1,8 +1,7 @@
 import socket
 import sys
-import subprocess
-import time
-import datetime
+import commands
+from time import time
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -12,14 +11,25 @@ server_address = ('0.0.0.0', 33153)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
+start_time = 0
+
 while True:
     data, address = sock.recvfrom(4096)
-    now = datetime.datetime.now()
-    mymin = (now.minute % 30)
+
+    end_time = time()
+    hours, rest = divmod(end_time,3600)
+    mymin, seconds = divmod(rest, 60)
+    mymin %= 30
+    
+    time_taken = end_time - start_time # time_taken is in seconds
+    hours, rest = divmod(time_taken,3600)
+    minutes, seconds = divmod(rest, 60)
+
     print "==>", data
     if data == "tss":
         pass #subprocess.call('/home/oem/Shanghai/closeJames.sh', shell=True)
-    elif mymin < 4:
+    elif mymin <= 4:
         pass
-    else:
-        subprocess.call('/home/oem/' + data + '.sh', shell=True)
+    elif minutes >= 4:
+        commands.getoutput('/home/oem/' + data + '.sh')
+        start_time = time()
