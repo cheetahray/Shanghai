b2f17eadfc11f4d59573384cb24ffca5312cc058
cheetahray <ray@dac.tw>
2016-12-31 02:14:12 +0800

    Change shutdmx to track16

--- osc.py
+++ osc.py
@@ -2,12 +2,14 @@ import OSC
 
 cc = OSC.OSCClient()
 cc.connect(('192.168.12.249', 7000))   # localhost, port 57120
-
+#cc.connect(('127.0.0.1', 6666))
 def click(msg):
     global cc
     oscmsg = OSC.OSCMessage()
     print "%s" % ("/track16/connect")
+    #print "%s" % ("/cue")
     oscmsg.setAddress("%s" % ("/track16/connect") )
+    #oscmsg.setAddress("%s" % ("/cue") )
     oscmsg.append(1)
     cc.send(oscmsg)
 
