5bcc7e80dcfae3c1b206addb0fb4dc9351231e0b
ray <ray@dac.tw>
2017-01-09 15:53:51 +0800

    Hope to solve network broke suddenly problem.

---
+++ ShanghaiHead.py
@@ -0,0 +1,10 @@
+import time
+import socket
+from struct import *
+
+port = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+for i in range(1,67):
+    port.sendto(pack('BB', 249, 3), ("192.168.12." + str(i), 5005) )
+    time.sleep(0.01)
+    
