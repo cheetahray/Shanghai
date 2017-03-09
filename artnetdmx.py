96dd69eff7c79a34187b89e608c408fee8b79570
ray <ray@dac.tw>
2017-03-09 11:36:12 +0800

    雷光夏~

---
+++ artnetdmx.py
@@ -0,0 +1,141 @@
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+import time
+
+from neopixel import *
+from twisted.internet import protocol, endpoints
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+# LED strip configuration:
+LED_COUNT      = 32      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+def artnetWipe(strip, color, i):
+    strip.setPixelColor(i, color)
+    strip.show()
+    #time.sleep(wait_ms/1000.0)
+
+# Define functions which animate LEDs in various ways.
+def colorWipe(strip, color, wait_ms=50):
+    """Wipe color across display a pixel at a time."""
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChase(strip, color, wait_ms=50, iterations=10):
+    """Movie theater light style chaser animation."""
+    for j in range(iterations):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, color)
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
+def rainbow(strip, wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(strip, wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+class ArtNet(DatagramProtocol):
+
+    def datagramReceived(self, data, (host, port)):
+        global strip
+        if ((len(data) > 18) and (data[0:8] == "Art-Net\x00")):
+            rawbytes = map(ord, data)
+            opcode = rawbytes[8] + (rawbytes[9] << 8)
+            protocolVersion = (rawbytes[10] << 8) + rawbytes[11]
+            if ((opcode == 0x5000) and (protocolVersion >= 14)):
+                sequence = rawbytes[12]
+                physical = rawbytes[13]
+                sub_net = (rawbytes[14] & 0xF0) >> 4
+                universe = rawbytes[14] & 0x0F
+                net = rawbytes[15]
+                rgb_length = (rawbytes[16] << 8) + rawbytes[17]
+                #print "seq %d phy %d sub_net %d uni %d net %d len %d" % \
+                #(sequence, physical, sub_net, universe, net, rgb_length)
+                idx = 18
+                x = 0
+                y = 0
+                while ((idx < (rgb_length+18)) and (y < 32)):
+                    r = rawbytes[idx]
+                    idx += 1
+                    g = rawbytes[idx]
+                    idx += 1
+                    b = rawbytes[idx]
+                    idx += 1
+                    artnetWipe(strip, Color(r, g, b), y) #unicorn.set_pixel(x, y, r, g, b)
+                    x += 1
+                    if (x >= 1):
+                        x = 0
+                        y += 1
+                #unicorn.show()
+strip = None
+# Main program logic follows:
+if __name__ == '__main__':
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+
+    print ('Press Ctrl-C to quit.')
+    '''
+    while True:
+        # Color wipe animations.
+        colorWipe(strip, Color(255, 0, 0))  # Red wipe
+        colorWipe(strip, Color(0, 255, 0))  # Blue wipe
+        colorWipe(strip, Color(0, 0, 255))  # Green wipe
+        # Theater chase animations.
+        theaterChase(strip, Color(127, 127, 127))  # White theater chase
+        theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+        theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+        # Rainbow animations.
+        rainbow(strip)
+        rainbowCycle(strip)
+        theaterChaseRainbow(strip)
+    '''
+    reactor.listenUDP(6454, ArtNet())
+    reactor.run()
